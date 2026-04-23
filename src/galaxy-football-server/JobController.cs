02dc66f6bc4bf31d2f6e735228605712b5785c6c
pvdhouten <pvdhouten@yahoo.com>
2026-04-23 18:31:37 -0300

    Add X-API-KEY to run job

--- src/galaxy-football-server/JobController.cs
+++ src/galaxy-football-server/JobController.cs
@@ -7,19 +7,21 @@ public class JobController : ControllerBase
 {
     private readonly ILogger<JobController> m_logger;
     private readonly JobService m_jobService;
+    private readonly IConfiguration m_configuration;
 
-    public JobController(ILogger<JobController> logger, JobService job_service)
+    public JobController(ILogger<JobController> logger, JobService job_service, IConfiguration configuration)
     {
-        m_logger     = logger;
-        m_jobService = job_service;
+        m_logger        = logger;
+        m_jobService    = job_service;
+        m_configuration = configuration;
     }
 
     [HttpPost("internal/run-daily-job")]
-    // public async Task<IActionResult> RunInternal((Name = "X-API-KEY")] string key)
-    public async Task<IActionResult> RunInternal()
+    public async Task<IActionResult> RunInternal(
+        [FromHeader(Name = "X-API-KEY")] string apiKey)
     {
-        //if (key != _config["InternalApiKey"])
-        //    return Unauthorized();
+        if (apiKey != m_configuration["GALAXY_FOOTBALL_API_KEY"])
+            return Unauthorized();
 
         await m_jobService.RunIfNeeded();
         return Ok();
